f8783155645e8c0cb68215037f1efe44eb1827a0
Shy Agam <us@bespunky.io>
2023-04-11 13:39:09 +0300

    ➕ Implemented attempt to load active file and position if not specified

--- src/features/debug/enso-debug.command.ts
+++ src/features/debug/enso-debug.command.ts
@@ -2,15 +2,40 @@ import * as vscode from 'vscode';
 import { Registerer } from '../../types/registerer';
 import { describeNodeIfGeneric, findNodeAtPosition } from '../../utils/typescript';
 import { createCommandUri } from '../../utils/commands';
+import { showNodeNotFoundMessage, showNodeNotDebuggableMessage } from './enso-debug.ui-utils';
 
 export const EnsoDebugCommandName = 'enso.debug';
 export type EnsoDebugCommandOrigin = 'palette' | 'hover' | 'external';
 
 export default function ensoDebugTypeCommand(context: vscode.ExtensionContext)
-{
-    return (sourceFilePath: string, line: number, character: number, origin: EnsoDebugCommandOrigin = 'external') =>
+{    
+    function attemptToProduceValidArgs(sourceFilePath?: string, line?: number, character?: number): { path: string; position: vscode.Position; }
     {
-        const nodeAtPosition = findNodeAtPosition(sourceFilePath, new vscode.Position(line, character));
+        if (sourceFilePath && line !== undefined && character !== undefined)
+            return { path: sourceFilePath, position: new vscode.Position(line, character) };
+        
+        const activeEditor = vscode.window.activeTextEditor;
+
+        if (!activeEditor)
+            throw new Error(`
+                Enso Debug failed to start.
+                No source file was specified and there is no active editor.
+                Strange... This should've been prevented by the 'when' clause of the contributions section.
+            `);
+        
+        const path = sourceFilePath ?? activeEditor.document.fileName;
+        const position = line && character
+            ? new vscode.Position(line, character)
+            : activeEditor.selection.start;
+        
+        return { path, position };
+    }
+
+    return (sourceFilePath?: string, line?: number, character?: number, origin: EnsoDebugCommandOrigin = 'palette') =>
+    {
+        const { path, position } = attemptToProduceValidArgs(sourceFilePath, line, character);
+
+        const nodeAtPosition = findNodeAtPosition(path, position);
 
         if (!nodeAtPosition)
         {
